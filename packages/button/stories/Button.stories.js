e19b294cfb7269f7595033302211cdffba6a0c2a
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-12-02 12:40:22 +0100

    chore: linted button package

--- packages/button/stories/Button.stories.js
+++ packages/button/stories/Button.stories.js
@@ -1,5 +1,5 @@
-import Button from "../src/Button.vue";
-import IconClose from "~icons/heroicons-solid/x";
+import Button from "../src/Button.vue"
+import IconClose from "~icons/heroicons-solid/x"
 
 export default {
   component: Button,
@@ -9,24 +9,25 @@ export default {
     color: "link",
     customColor: "",
     disabled: false,
-    withIcon: false
+    withIcon: false,
   },
   argTypes: {
     default: {
       controls: {
-        type: "text"
+        type: "text",
       },
       table: {
-        category: "Slots"
-      }
+        category: "Slots",
+      },
     },
     icon: {
-      name: 'Icon slot',
+      name: "Icon slot",
       control: null,
       table: {
-        category: 'Slots'
+        category: "Slots",
       },
-      description: 'Use this slot to put icon into button. But remember about adding with-icon prop to enable rendering it'
+      description:
+        "Use this slot to put icon into button. But remember about adding with-icon prop to enable rendering it",
     },
     color: {
       options: ["primary", "success", "danger", "info", "warning", "link"],
@@ -34,56 +35,58 @@ export default {
       control: { type: "select" },
       table: {
         category: "Props",
-        defaultValue: { summary: 'link' },
+        defaultValue: { summary: "link" },
       },
-      description: "Through this property you can select built-in color theme or define yours putting here CSS classes which describe text & background color"
+      description:
+        "Through this property you can select built-in color theme or define yours putting here CSS classes which describe text & background color",
     },
     withIcon: {
       type: {
         name: "boolean",
-        required: false
+        required: false,
       },
       table: {
         category: "Props",
         defaultValue: { summary: false },
       },
-      description: "This boolean flag control adding extra slot #icon though which you can put icon into button. This slot is wrapped with extra div which have CSS styles to align icon to center both axes."
+      description:
+        "This boolean flag control adding extra slot #icon though which you can put icon into button. This slot is wrapped with extra div which have CSS styles to align icon to center both axes.",
     },
     disabled: {
       type: {
-        name: 'boolean',
-        required: false
+        name: "boolean",
+        required: false,
       },
       table: {
-        category: 'Props',
+        category: "Props",
         defaultValue: { summary: false },
       },
-      description: 'Simple boolean flag to make button disabled'
+      description: "Simple boolean flag to make button disabled",
     },
     customColor: {
       control: { type: "text" },
-      description: 'Additional field created only in Custom Color Story to handle declaring own color.'
+      description: "Additional field created only in Custom Color Story to handle declaring own color.",
     },
     onClick: {
-      name: 'On click event',
-      action: 'click',
+      name: "On click event",
+      action: "click",
       table: {
-        category: "Events"
+        category: "Events",
       },
-      description: "Simple event when you click on it"
-    }
+      description: "Simple event when you click on it",
+    },
   },
   parameters: {
     docs: {
       page: null,
-    }
-  }
-};
+    },
+  },
+}
 
-const Template = (args) => ({
+const Template = args => ({
   components: { Button, IconClose },
   setup() {
-    return { args };
+    return { args }
   },
   template: `
     <Button v-bind="args">
@@ -91,42 +94,42 @@ const Template = (args) => ({
       <IconClose />
     </template>
     {{ args.default }}
-    </Button>`
-});
+    </Button>`,
+})
 
-export const Link = Template.bind({});
+export const Link = Template.bind({})
 
-export const Primary = Template.bind({});
-Primary.args = { color: "primary" };
+export const Primary = Template.bind({})
+Primary.args = { color: "primary" }
 
-export const Success = Template.bind({});
-Success.args = { color: "success" };
+export const Success = Template.bind({})
+Success.args = { color: "success" }
 
-export const Danger = Template.bind({});
-Danger.args = { color: "danger" };
+export const Danger = Template.bind({})
+Danger.args = { color: "danger" }
 
-export const Warning = Template.bind({});
-Warning.args = { color: "warning" };
+export const Warning = Template.bind({})
+Warning.args = { color: "warning" }
 
-export const Info = Template.bind({});
-Info.args = { color: "info" };
+export const Info = Template.bind({})
+Info.args = { color: "info" }
 
-export const Disabled = Template.bind({});
+export const Disabled = Template.bind({})
 Disabled.args = {
   color: "primary",
-  disabled: true
-};
+  disabled: true,
+}
 
-export const WithIcon = Template.bind({});
+export const WithIcon = Template.bind({})
 WithIcon.args = {
   withIcon: true,
-  color: "primary"
-};
+  color: "primary",
+}
 
-const CustomTemplate = (args) => ({
+const CustomTemplate = args => ({
   components: { Button, IconClose },
   setup() {
-    return { args };
+    return { args }
   },
   template: `
     <Button :color="args.customColor.length > 0 ? args.customColor : args.color" :with-icon="args.withIcon">
@@ -134,10 +137,11 @@ const CustomTemplate = (args) => ({
       <IconClose />
     </template>
     {{ args.default }}
-    </Button>`
-});
-export const CustomColor = CustomTemplate.bind({});
+    </Button>`,
+})
+export const CustomColor = CustomTemplate.bind({})
 CustomColor.args = {
   withIcon: true,
-  customColor: "px-3 py-2 bg-green-600 text-white focus:ring-2 focus:ring-offset-2 focus:ring-green-500 hover:bg-green-700"
-};
+  customColor:
+    "px-3 py-2 bg-green-600 text-white focus:ring-2 focus:ring-offset-2 focus:ring-green-500 hover:bg-green-700",
+}
