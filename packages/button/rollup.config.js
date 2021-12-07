beb93391496c0bbb0ad52e8114b7fe2975669045
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-12-07 09:52:17 +0100

    feat: created rollup, babel & windi config in button

---
+++ packages/button/rollup.config.js
@@ -0,0 +1,26 @@
+const {createBanner, createConfig} = require("@grixu/rollup-config")
+const pkg = require('./package.json')
+
+const banner = createBanner(pkg)
+
+const outputConfigs = {
+  esm: {
+    file: pkg.module,
+    format: `es`,
+  },
+  cjs: {
+    file: pkg.main,
+    format: `cjs`,
+  },
+  browser: {
+    file: pkg.browser,
+    format: 'umd',
+    minify: true,
+    css: true
+  },
+}
+
+const allFormats = Object.keys(outputConfigs)
+const packageConfigs = allFormats.map((format) => createConfig(format, outputConfigs[format], banner, pkg))
+
+export default packageConfigs
---
+++ packages/button/windi.config.ts
@@ -0,0 +1,3 @@
+import config from '../../windi.config'
+
+export default config
