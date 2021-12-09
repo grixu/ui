1779195690667cf445314111bacf0cc0e41c3612
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-12-09 11:02:01 +0100

    feat: added using vue-use-variants in Button

--- packages/button/src/Button.vue
+++ packages/button/src/Button.vue
@@ -1,20 +1,7 @@
 <template>
   <button
     :disabled="disabled"
-    :class="[css, disabled ? 'opacity-50 cursor-not-allowed' : '']"
-    class="
-      flex
-      cursor-pointer
-      group
-      transition-all
-      border border-transparent
-      focus:outline-none
-      mx-1
-      font-medium font-base
-      rounded-md
-      items-center
-      content-center
-    "
+    :class="css"
     data-testid="button"
     @click.stop="emitClick"
   >
@@ -29,55 +16,27 @@
 
 <script setup lang="ts">
 import { computed, toRefs, useSlots } from "vue"
+import {variantsDefaults, variants} from "./variants"
+import {UseVariant, useVariant} from "vue-use-variant";
 
 interface ButtonProps {
-  color?: "link" | "primary" | "success" | "danger" | "warning" | "info"
+  link?: boolean
+  primary?: boolean
+  success?: boolean
+  danger?: boolean
+  warning?: boolean
+  info?: boolean
   disabled?: boolean
   withIcon?: boolean
 }
 
 const props = withDefaults(defineProps<ButtonProps>(), {
-  color: "link",
-  disabled: false,
+  ...variantsDefaults,
   withIcon: false,
 })
 
-const { color, disabled } = toRefs(props)
-
-const css = computed(() => {
-  const baseCommonCss = "px-3 py-2"
-
-  switch (color.value) {
-    case "link":
-      return "focus:underline focus:text-primary hover:text-primary hover:underline"
-    case "success":
-      return (
-        baseCommonCss +
-        " bg-green-600 text-white focus:ring-2 focus:ring-offset-2 focus:ring-green-500 hover:bg-green-700"
-      )
-    case "danger":
-      return (
-        baseCommonCss + " bg-red-600 text-white focus:ring-2 focus:ring-offset-2 focus:ring-red-500 hover:bg-red-700"
-      )
-    case "info":
-      return (
-        baseCommonCss +
-        " bg-white text-black border border-gray-300 focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 hover:bg-gray-100"
-      )
-    case "warning":
-      return (
-        baseCommonCss +
-        " bg-yellow-600 text-white focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 hover:bg-yellow-700"
-      )
-    case "primary":
-      return (
-        baseCommonCss +
-        " bg-primary text-white focus:ring-2 focus:ring-offset-2 focus:ring-primary-dark hover:bg-primary-darker"
-      )
-    default:
-      return color.value
-  }
-})
+const { defineVariant } = useVariant() as UseVariant
+const css = defineVariant({ default: true, ...props }, variants)
 
 const slots = useSlots()
 const isText = computed<boolean>(() => {
@@ -87,6 +46,7 @@ const isText = computed<boolean>(() => {
 const emit = defineEmits<{
   (e: "click"): void
 }>()
+const { disabled } = toRefs(props)
 const emitClick = () => {
   if (!disabled.value) emit("click")
 }
