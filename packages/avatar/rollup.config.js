c08228e7b64f87e85d3b4d8884582659d7c57d93
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-12-13 12:18:46 +0100

    feat: added configs (TS, ESLint, Stylelint, Rollup) + package.json updated for build-ready

---
+++ packages/avatar/rollup.config.js
@@ -0,0 +1,30 @@
+const { createBanner, createConfig } = require("@grixu/rollup-config")
+const pkg = require("./package.json")
+
+const banner = createBanner(pkg)
+
+const outputConfigs = {
+  esm: {
+    file: pkg.module,
+    format: `es`,
+  },
+  cjs: {
+    file: pkg.main,
+    format: `cjs`,
+  },
+  browser: {
+    file: pkg.browser,
+    format: "umd",
+    minify: true,
+    css: true,
+  },
+  css: {
+    file: pkg.style,
+    minify: true,
+  },
+}
+
+const allFormats = Object.keys(outputConfigs)
+const packageConfigs = allFormats.map(format => createConfig(format, outputConfigs[format], banner, pkg))
+
+export default packageConfigs
--- packages/avatar/stories/Avatar.stories.js
+++ packages/avatar/stories/Avatar.stories.js
@@ -1,95 +1,98 @@
-import Avatar from "../src/Avatar.vue";
-import IconTrash from '~icons/heroicons-solid/trash'
+import Avatar from "../src/Avatar.vue"
+import IconTrash from "~icons/heroicons-solid/trash"
 
 export default {
   component: Avatar,
   title: "components/Avatar",
   args: {
     img: "",
-    text: ""
+    text: "",
   },
   argTypes: {
     text: {
       name: "Text",
       type: {
         name: "string",
-        required: false
+        required: false,
       },
       table: {
         category: "Props",
-        defaultValue: { summary: "John Doe" }
+        defaultValue: { summary: "John Doe" },
       },
-      description: "Text value from which component generating initials when image is not available"
+      description: "Text value from which component generating initials when image is not available",
     },
     img: {
       name: "Avatar's URL",
       type: {
-        name: 'string',
-        required: false
+        name: "string",
+        required: false,
       },
       table: {
         category: "Props",
-        defaultValue: { summary: 'https://via.placeholder.com/150' }
+        defaultValue: { summary: "https://via.placeholder.com/150" },
       },
-      description: "Avatar's URL - just it. But if you not provide URL, you should provide alternative text, to generate initials on avatar"
+      description:
+        "Avatar's URL - just it. But if you not provide URL, you should provide alternative text, to generate initials on avatar",
     },
     default: {
-      name: 'Default Slot',
+      name: "Default Slot",
       table: {
-        category: "Slots"
+        category: "Slots",
       },
       control: {
-        type: 'text'
+        type: "text",
       },
-      description: "If you don't pass either url or text, component just show icon. Of course it's adjustable - by this slot you can provide your own icon."
+      description:
+        "If you don't pass either url or text, component just show icon. Of course it's adjustable - by this slot you can provide your own icon.",
     },
     onClick: {
-      name: 'On click event',
-      action: 'click',
+      name: "On click event",
+      action: "click",
       table: {
-        category: "Events"
+        category: "Events",
       },
-      description: "Simple event when you click on it"
-    }
+      description: "Simple event when you click on it",
+    },
   },
-  decorators: [
-    () => ({ template: "<div style=\"margin: 3em;\"><story/></div>" })
-  ],
+  decorators: [() => ({ template: '<div style="margin: 3em;"><story/></div>' })],
   docs: {
     page: null,
-  }
-};
+  },
+}
 
-const Template = (args) => ({
+const Template = args => ({
   components: { Avatar },
   setup() {
     return { args }
   },
   template: `
-    <Avatar v-bind="args"></Avatar>`
-});
+    <Avatar v-bind="args"></Avatar>`,
+})
 
-export const Text = Template.bind({});
+export const Text = Template.bind({})
 Text.args = {
-  text: "John Doe"
-};
+  text: "John Doe",
+}
 
 export const VeryLongText = Template.bind({})
 VeryLongText.args = {
-  text: 'This Is A Very Long Text'
+  text: "This Is A Very Long Text",
 }
 
-export const Image = Template.bind({});
+export const Image = Template.bind({})
 Image.args = {
-  img: "https://www.polskieprzetwornice.pl/sklep/img/avatar3.png"
-};
+  img: "https://www.polskieprzetwornice.pl/sklep/img/avatar3.png",
+}
 
-export const DefaultIcon = Template.bind({});
+export const DefaultIcon = Template.bind({})
 
-export const Custom = (args) => ({
+export const Custom = args => ({
   components: { Avatar, IconTrash },
+  setup() {
+    return { args }
+  },
   template: `
     <Avatar>
       <IconTrash />
-    </Avatar>`
-});
+    </Avatar>`,
+})
